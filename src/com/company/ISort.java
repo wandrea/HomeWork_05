4b29e4dd627b501e8046d471e1c8a4672be72a2c
wandrea <wagenhofferandrea@gmail.com>
2017-12-14 20:26:05 +0100

    #1

---
+++ src/com/company/ISort.java
@@ -0,0 +1,14 @@
+package com.company;
+
+/**
+ * Created by Andrea on 2017.12.14..
+ */
+public interface ISort {
+    void bubbleSort(int[] matrix);
+    void insertionSort(int[] matrix);
+    void selectionSort(int[] matrix);
+    void mergeSort(int[] matrix);
+    void quickSort(int[] matrix);
+
+
+}
---
+++ src/com/company/Sort.java
@@ -0,0 +1,53 @@
+package com.company;
+
+/**
+ * Created by Andrea on 2017.12.14..
+ */
+public class Sort implements ISort {
+    @Override
+    public void bubbleSort(int[] array) {
+        //mivel az UML-ben csak egy tömb a bemeneti paraméter, így  nem volt
+        //egyértelmûsítve, hogy csökkenõ vagy növekvõ sorrendbe rendezze,
+        //ezért úgy döntöttem, hogy alapértelmezetten növekvõ rendezés lesz.
+
+        for (int i = array.length - 1; i >= 1; i--) {
+
+           // int swaps = 0;
+
+            for (int j = 0; j <= i - 1; j++) {
+
+                if (array[j] > array[j + 1]) {
+                    int temp = array[j];
+                    array[j] = array[j + 1];
+                    array[j + 1] = temp;
+                   // ++swaps;
+                }
+            }
+
+           // if (swaps == 0) {
+               // break;}
+        }
+    }
+
+    @Override
+    public void insertionSort(int[] array) {
+
+    }
+
+    @Override
+    public void selectionSort(int[] array) {
+
+    }
+
+    @Override
+    public void mergeSort(int[] array) {
+
+    }
+
+    @Override
+    public void quickSort(int[] array) {
+
+    }
+
+
+}
